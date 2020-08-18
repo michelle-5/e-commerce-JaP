152feb9fdb4a74fcb80df87f7c63e1c988dcb648
michelle-5 <michellelazcano13@gmail.com>
2020-08-18 13:42:26 -0300

    oauth

--- js/init.js
+++ js/init.js
@@ -69,6 +69,14 @@ function onSignIn(googleUser) {
   $("email").text(profile.getEmail());
 }
 
+function signOut(){
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function(){
+    $("g-signin2").css("display", "block");
+    $("data").css("display", "none");
+  });
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
