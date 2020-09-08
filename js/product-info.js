c64fba4626dc1d7fd9a5a9fb84878f8c73dfdef4
michelle-5 <michellelazcano13@gmail.com>
2020-09-08 15:53:37 -0300

    added product desc and comments

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,75 @@
+var currentProductsArray = [];
+var currentCommentsArray = [];
+var product = {};
+
+function showImages(array) {
+	let htmlContentToAppend = "";
+
+	for (let i = 0; i < array.length; i++) {
+		let imageSrc = array[i];
+
+		htmlContentToAppend +=
+        `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div> `;
+
+		document.getElementById("productImages").innerHTML = htmlContentToAppend;
+	}
+}
+
+function showComments(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let comment = array[i];   
+
+            htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        ` + comment.user +`
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ comment.score +`</h4>
+                            <small class="text-muted">` + comment.dateTime + ` </small>
+                        </div>
+                        <p class="mb-1">` + comment.description + `</p>
+                    </div>
+                </div>
+            </div>
+            `
+        document.getElementById("comments-container").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {product = resultObj.data;
+            document.getElementById("productName").innerHTML = product.name;
+            document.getElementById("productDesc").innerHTML = product.description;
+            document.getElementById("productCost").innerHTML = product.currency + " " + product.cost;
+            document.getElementById("productSoldCount").innerHTML = product.soldCount;
+            document.getElementById("productCategory").innerHTML = product.category;
+            showImages(product.images);
+        }
+    });
+});
 
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            currentCommentsArray = resultObj.data;
+            showComments(currentCommentsArray);
+        }
+    });
 });
